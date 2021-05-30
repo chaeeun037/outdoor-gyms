1db2c471e953379ebafe74809c2256836d8de447
Chaeeun Kim <fairy037@naver.com>
2021-05-30 20:25:37 +0900

    marker 추가

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
-import { RenderAfterNavermapsLoaded, NaverMap } from 'react-naver-maps'
+import { RenderAfterNavermapsLoaded, NaverMap, Marker } from 'react-naver-maps'
 
 function NaverMapsAPI() {
+  const navermaps = window.naver.maps;
   return (
     <NaverMap 
       id='maps-examples-map-simple'
@@ -10,7 +11,17 @@ function NaverMapsAPI() {
         width: '100%',
         height: '600px',
       }}
-    />
+      defaultCenter={{ lat: 37.3361804, lng: 127.124099 }}
+      defaultZoom={16}
+    >
+      <Marker 
+        position={new navermaps.LatLng(37.3361804, 127.124099)}
+        animation={navermaps.Animation.BOUNCE}
+        onClick={() => {
+          alert('여기는 우리집 입니다.')
+        }}
+      />
+    </NaverMap>
   )
 }
 
