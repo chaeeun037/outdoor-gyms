c5eb77df0df49c6846f50e0c72b02f7889903b04
Chaeeun Kim <fairy037@naver.com>
2021-05-30 20:12:56 +0900

    react naver maps install

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import { RenderAfterNavermapsLoaded, NaverMap } from 'react-naver-maps'
+
+function NaverMapsAPI() {
+  return (
+    <NaverMap 
+      id='maps-examples-map-simple'
+      style={{
+        width: '100%',
+        height: '600px',
+      }}
+    />
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RenderAfterNavermapsLoaded
+      ncpClientId={'mu55qgbh9l'}
+      error={<p>Maps Load Error</p>}
+      loading={<p>Maps Loading...</p>}
+    >
+      <NaverMapsAPI />
+    </RenderAfterNavermapsLoaded>
   );
 }
 
