411dbecfaee93ee66ad52a10969231f767b5f98a
Chaeeun Kim <fairy037@naver.com>
2021-05-30 21:27:47 +0900

    map marker 이동 구현

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import { RenderAfterNavermapsLoaded, NaverMap, Marker } from 'react-naver-maps'
+import { useState } from 'react'
 
 function NaverMapsAPI() {
   const navermaps = window.naver.maps;
+  const [center, setCenter] = useState({ lat: 37.3361804, lng: 127.124099 })
+
+  const handleCenterChanged = (changedCenter) => {
+    setCenter({lat: changedCenter.y, lng: changedCenter.x});
+  }
+
   return (
     <NaverMap 
       id='maps-examples-map-simple'
@@ -11,14 +18,15 @@ function NaverMapsAPI() {
         width: '100%',
         height: '600px',
       }}
-      defaultCenter={{ lat: 37.3361804, lng: 127.124099 }}
+      defaultCenter={center}
+      onCenterChanged={handleCenterChanged}
       defaultZoom={16}
     >
       <Marker 
-        position={new navermaps.LatLng(37.3361804, 127.124099)}
+        position={new navermaps.LatLng(center.lat, center.lng)}
         animation={navermaps.Animation.BOUNCE}
         onClick={() => {
-          alert('여기는 우리집 입니다.')
+          alert('마커!')
         }}
       />
     </NaverMap>
